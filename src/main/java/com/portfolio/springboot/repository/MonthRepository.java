76dd68fdd8468da3b9af92d80aec177005da7ad5
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2022-02-11 17:20:10 -0300

    removed month

--- src/main/java/com/portfolio/springboot/repository/MonthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portfolio.springboot.repository;
-
-import com.portfolio.springboot.dto.response.MonthDtoResponse;
-import com.portfolio.springboot.generic.GenericRepository;
-import com.portfolio.springboot.model.Month;
-
-public interface MonthRepository extends GenericRepository<Month, MonthDtoResponse> {
-}
